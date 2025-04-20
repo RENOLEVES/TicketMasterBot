f154b8cf612a7fffe7a0a642283535f1e07250a0
RENOLEVES <zzhao260985652@gmail.com>
2025-04-20 12:56:53 -0400

    Update populate result table

---
+++ task.py
@@ -0,0 +1,18 @@
+import subprocess
+import time
+
+for i in range(1):
+    print(f"\n--- Iteration {i+1} ---")
+
+    # Run main.py
+    subprocess.run(["python", "main.py"])
+
+    # Run process.py
+    subprocess.run(["python", "process.py"])
+
+    # # Wait for 5 minutes (300 seconds)
+    # if i < 9:  # No need to wait after the last iteration
+    #     print("Waiting 5 minutes...")
+    #     time.sleep(300)
+
+print("\nDone.")
