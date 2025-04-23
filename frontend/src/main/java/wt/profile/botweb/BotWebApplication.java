f2ded173baca1c70c0eeb7f593409546932b1974
RENOLEVES <zzhao260985652@gmail.com>
2025-04-23 18:51:12 -0400

    update frontend and backend structure

---
+++ frontend/src/main/java/wt/profile/botweb/BotWebApplication.java
@@ -0,0 +1,13 @@
+package wt.profile.botweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotWebApplication.class, args);
+	}
+
+}
---
+++ frontend/src/test/java/wt/profile/botweb/BotWebApplicationTests.java
@@ -0,0 +1,13 @@
+package wt.profile.botweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
